cd76d4f43621c910b3af84f696c54092baed3e85
Brent Quackenbush <brentprogramming@gmail.com>
2023-02-05 16:04:26 -0600

    Use Composite literal to create array

---
+++ practiceexercise4/Exercise1/Exercise1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+ Using a COMPOSITE LITERAL:
+● create an ARRAY which holds 5 VALUES of TYPE int
+● assign VALUES to each index position.
+● Range over the array and print the values out.
+● Using format printing
+○ print out the TYPE of the array
+*/
+
+func main() {
+	x := []int{1, 2, 3, 4, 5}
+	for i, v := range x {
+		fmt.Println(i, v)
+	}
+}
