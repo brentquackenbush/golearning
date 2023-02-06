8d66ef755a62a026157ca18d357bd204baf1573f
Brent Quackenbush <brentprogramming@gmail.com>
2023-02-05 19:19:16 -0600

    Rename directory

---
+++ practiceexercises4/Exercise9/Exercise9.go
@@ -0,0 +1,6 @@
+package Exercise9
+
+/*
+Using the code from the previous example, add a record to your map. Now print the map out using the “range” loop
+   Todd McLeod - Learn To Code Go on Udemy - Part 1 - Page 45
+*/
