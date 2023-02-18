0dcb28614d087bd1d04d1bca7afee6d072d943f9
Brent Quackenbush <brentprogramming@gmail.com>
2023-02-18 14:39:25 -0600

    functions

---
+++ practiceexercises6/Exercise1/Exercise1.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+○ create a func with the identifier foo that returns an int
+○ create a func with the identifier bar that returns an int and a string
+○ call both funcs
+○ print out their results
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(foo())
+	fmt.Println(bar())
+}
+
+func foo() int {
+	return 1
+}
+
+func bar() (int, string) {
+	return 2, "test"
+}
