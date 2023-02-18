111f90d1688870968b1bf530795013f01648bc09
Brent Quackenbush <brentprogramming@gmail.com>
2023-02-18 14:59:40 -0600

    anonymous func

---
+++ practiceexercises6/Exercise5/Exercise6.go
@@ -0,0 +1,18 @@
+package main
+
+//Build and use an anonymous func
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println(i)
+		}
+	}()
+
+	fmt.Println("done")
+}
