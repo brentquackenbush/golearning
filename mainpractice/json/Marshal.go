1105603ddb291ef020550c8ea200396e27605c76
Brent Quackenbush <brentprogramming@gmail.com>
2023-02-21 19:00:06 -0600

    anonymous func

--- mainpractice/json/Marshal.go
+++ mainpractice/json/Marshal.go
@@ -1,1 +1,39 @@
-package json
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+
+	p1 := person{
+		First: "Brent",
+		Last:  "Junior",
+		Age:   23,
+	}
+	p2 := person{
+		First: "Taco",
+		Last:  "Spice",
+		Age:   27,
+	}
+
+	people := []person{p1, p2}
+
+	fmt.Println(people)
+
+	//to send data (encode) into json. It is called Marshalling in Go
+
+	bs, err := json.Marshal(people)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(bs))
+}
